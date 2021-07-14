1e4e98d8420caf5b7f6cbd84d3a73d10c6f906fa
Danny Rhee <47438772+Gimorhee@users.noreply.github.com>
2021-07-14 05:43:58 -0700

    edit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get("/", (req, res) => {
   res.send("API Running");
 });
 
-// Defined Routese
+// Defined Routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
