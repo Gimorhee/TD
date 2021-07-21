32857a860ff81794a54cb45dd5cc2fd39dae1f81
Danny Rhee <47438772+Gimorhee@users.noreply.github.com>
2021-07-20 22:25:57 -0700

    Add / Delete comment on post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -154,4 +154,80 @@ router.put("/unlike/:post_id", auth, async (req, res) => {
   }
 });
 
+// @route POST api/posts/comment/:post_id
+// @desc Comment on a post
+// @access Private
+router.post("/comment/:post_id", [auth, check("text", "Text is required").not().isEmpty()], async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    const post = await Post.findById(req.params.post_id);
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    const newComment = {
+      user: req.user.id,
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+    };
+
+    post.comments.unshift(newComment);
+
+    await post.save();
+
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    res.status(500).send("Sever Error!");
+  }
+});
+
+// @route DELETE api/posts/comment/:post_id/:comment_id
+// @desc Delete a comment on a post by its id
+// @access Private
+router.delete("/comment/:post_id/:comment_id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.post_id);
+
+    const theComment = post.comments.filter((comment) => comment._id.toString() === req.params.comment_id);
+
+    // Check if there is a comment
+    if (theComment.length === 0) {
+      return res.status(400).json({ msg: "Comment not found" });
+    }
+
+    // Check user authorization
+    if (theComment[0].user.toString() !== req.user.id) {
+      return res.status(400).json({ msg: "User not authorized" });
+    }
+
+    post.comments = post.comments.filter((comment) => comment._id.toString() !== req.params.comment_id);
+
+    await post.save();
+
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    res.status(500).send("Sever Error!");
+  }
+});
+
 module.exports = router;
