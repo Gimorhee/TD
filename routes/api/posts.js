5c018c1e55c0ba5b5816c8bce126329c72e4fcce
Danny Rhee <47438772+Gimorhee@users.noreply.github.com>
2021-07-20 20:25:55 -0700

    Create Post / Get all posts routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,54 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
+const PetProfile = require("../../models/PetProfile");
+const User = require("../../models/User");
+const Post = require("../../models/Post");
 
-// @route GET api/prosts
-// @desc Test Route
-// @access Public
-router.get("/", (req, res) => {
-  res.send("prosts Route");
+// @route GET api/posts
+// @desc Get all posts
+// @access Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Post.find();
+
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Sever Error!");
+  }
+});
+
+// @route POST api/posts
+// @desc Create a post
+// @access Private
+router.post("/", [auth, [check("text", "Text is required").not().isEmpty()]], async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+
+    let postObj = {
+      user: req.user.id,
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+    };
+
+    let post = await new Post(postObj);
+
+    await post.save();
+
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Sever Error!");
+  }
 });
 
 module.exports = router;
