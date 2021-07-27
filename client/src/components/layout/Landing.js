18576e256e39b8c0ae2ae59a92eb75f2f53e2b44
Danny Rhee <47438772+Gimorhee@users.noreply.github.com>
2021-07-26 19:02:36 -0700

    Testimonial component UI

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,8 +1,25 @@
 import React from "react";
 import iphoneImg from "../../assets/image/iphone6.png";
-import { Button, Icon, Label } from "semantic-ui-react";
+import dogImg1 from "../../assets/image/cutedog1.jpg";
+import dogImg2 from "../../assets/image/dog-img.jpg";
+import dogImg3 from "../../assets/image/cutedog.jpg";
+
+import { Button } from "semantic-ui-react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Landing = () => {
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
+
   return (
     <div className="landing">
       <section className="hero">
@@ -40,7 +57,31 @@ const Landing = () => {
           <p>Find the true love of your dog's life!</p>
         </div>
       </section>
-      <section className="testimonial">testimonial</section>
+      <section className="testimonial">
+        <Slider {...settings} className="carousel">
+          <div>
+            <h3>I no longer have to sniff other dogs for love. I've found the hottest Retriever on TinDog. Woof!</h3>
+            <div className="image">
+              <img src={dogImg1} alt="" />
+            </div>
+            <span>Benji, Vancouver</span>
+          </div>
+          <div>
+            <h3>As you can see from the picture below, I am living the life!</h3>
+            <div className="image">
+              <img src={dogImg3} alt="" />
+            </div>
+            <span>Timon, Vancouver</span>
+          </div>
+          <div>
+            <h3>Thanks to TinDog, I met true love of my life! Woof!</h3>
+            <div className="image">
+              <img src={dogImg2} alt="" />
+            </div>
+            <span>Simon, Coquitlam</span>
+          </div>
+        </Slider>
+      </section>
     </div>
   );
 };
