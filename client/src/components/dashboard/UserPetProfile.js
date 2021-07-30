7997eacd9c79cac9e8a239db86fdc8f84cd44f47
Danny Rhee <47438772+Gimorhee@users.noreply.github.com>
2021-07-29 23:52:06 -0700

    UserPetProfile UI works

--- client/src/components/dashboard/UserPetProfile.js
+++ client/src/components/dashboard/UserPetProfile.js
@@ -1,28 +1,69 @@
-import React, { Fragment } from "react";
-import { Card } from "semantic-ui-react";
+import React, { Fragment, useEffect } from "react";
+import { Card, Divider } from "semantic-ui-react";
 // import PropTypes from "prop-types";
 
 const UserPetProfile = ({ user, profile }) => {
+  useEffect(() => {
+    profile && profile.characteristics.map((characteristic) => console.log(characteristic));
+  }, []);
+
   return (
     <section className="userPetProfile">
       <Card className="outerCard">
         <div className="infoContainer">
           {profile !== null && (
             <Fragment>
-              {" "}
-              <h3>
-                <span>{user && user.name}</span> & <span>{profile && profile.name}</span>'s Profile
-              </h3>
               <div className="info">
-                <div className="avatar">
-                  <img src={user && user.avatar} alt="dog-avatar-img" />
+                <div className="intro">
+                  <h1>
+                    {user && user.name} & {profile && profile.name}
+                    <span>'s</span>
+                  </h1>
+
+                  <div className="avatar">
+                    <img src={user && user.avatar} alt="dog-avatar-img" />
+                  </div>
+
+                  <div className="sns"></div>
+
+                  <div className="bubblyBox">
+                    <p className="description">{profile && profile.description}</p>
+                  </div>
                 </div>
-                <div>BECAME MEMBER: {user && user.date}</div>
-                <div>AGE: {profile && profile.age}</div>
-                <div>BREED: {profile && profile.breed}</div>
-                <div>GENDER: {profile && profile.gender}</div>
-                <div>DESCRIPTION: {profile && profile.description}</div>
-                <div>LOCATION: {profile && profile.location}</div>
+
+                <Divider className="bigDivider" />
+
+                {/* <div>BECAME MEMBER: {user && user.date}</div> */}
+                <Card className="about">
+                  <div className="detail">
+                    <h3>AGE:</h3>
+                    <p>{profile && profile.age}</p>
+                  </div>
+                  <Divider className="smallDivider" />
+
+                  <div className="detail">
+                    <h3>BREED: </h3>
+                    <p>{profile && profile.breed}</p>
+                  </div>
+                  <Divider className="smallDivider" />
+
+                  <div className="detail">
+                    <h3>GENDER:</h3>
+                    <p>{profile && profile.gender}</p>
+                  </div>
+                  <Divider className="smallDivider" />
+
+                  <div className="detail">
+                    <h3>LOCATION:</h3>
+                    <p>{profile && profile.location}</p>
+                  </div>
+                  <Divider className="smallDivider" />
+
+                  <ul className="characteristics">
+                    <h3>CHARACTERISTICS:</h3>
+                    {profile && profile.characteristics.map((characteristic, i) => <li key={`characteristic-${i}`}>{characteristic}</li>)}
+                  </ul>
+                </Card>
               </div>
             </Fragment>
           )}
