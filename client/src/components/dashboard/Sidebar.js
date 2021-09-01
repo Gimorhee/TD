54f7279df2dbe1f20d34eed712be7cedfec3a862
Danny Rhee <47438772+Gimorhee@users.noreply.github.com>
2021-09-01 02:58:19 -0700

    Sidebar : leaderboard logic

--- client/src/components/dashboard/Sidebar.js
+++ client/src/components/dashboard/Sidebar.js
@@ -1,14 +1,29 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import { Card, Feed } from "semantic-ui-react";
 import { Link } from "react-router-dom";
-import dogImg1 from "../../assets/image/cutedog.jpg";
-import dogImg2 from "../../assets/image/dog-img.jpg";
 
 import { withRouter } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import AllPetProfiles from "./Main/AllPetProfiles";
 
-// import PropTypes from "prop-types";
+const Sidebar = ({ logout, history, auth, petProfile, post }) => {
+  const [leaderboard, setLeaderBoard] = useState([]);
+  useEffect(() => {
+    // petProfile.profiles.map((profile) => {
+    //     arr.push({
+    //         userId: profile.user._id,
+    //         user: profile.user.name,
+    //         pet: profile.name,
 
-const Sidebar = ({ logout, history }) => {
+    //     })
+    // })
+    // arr = petProfile.profiles.sort((a, b) => (a.likes.length > b.likes.length ? 1 : -1));
+
+    setLeaderBoard(petProfile.profiles.sort((a, b) => (a.likes.length < b.likes.length ? 1 : -1)));
+    console.log("=> ", leaderboard);
+  }, [petProfile]);
+
   return (
     <section className="sidebar">
       <Card className="outerCard">
@@ -25,186 +40,36 @@ const Sidebar = ({ logout, history }) => {
           </span>
         </div>
 
-        {/* HEADER  */}
         <Card.Content className="headerContainer">
           <Card.Header>Recent Posts</Card.Header>
         </Card.Content>
 
-        {/* CONTENT */}
-        <Card.Content className="contentContainer">
-          <Feed>
-            <Feed.Event style={{ background: "#FEEFD0" }}>
-              <Feed.Label image={dogImg1} />
-              <Feed.Content>
-                <Feed.Date content="1 days ago" />
-                <Feed.Summary>What the woof?</Feed.Summary>
-              </Feed.Content>
-              <div className="usersCommented">
-                <div className="avatar">
-                  <img src={dogImg2} alt="user-avatar" />
-                </div>
-                <div className="avatar">
-                  <img src="https://cuteprofilepictures.weebly.com/uploads/2/1/8/5/21856578/2598313_orig.jpg" alt="user-avatar" />
-                </div>
-                <div className="avatar">
-                  <img
-                    src="https://media1.popsugar-assets.com/files/thumbor/zpzsOsdUEptenTm_qy6IUBOroto/0x0:1660x1660/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2019/10/29/927/n/44701584/0fd997f65db8abf1796548.77940914_/i/short-funny-pet-videos.png"
-                    alt="user-avatar"
-                  />
-                </div>
-                <div className="avatar">
-                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9W2_KnQG4R-BVSWNpTTJCueKLcjFbwhCsnT3KI92Yhx3B3ZdKrjdzcEeuVWR8y32PaxA&usqp=CAU" alt="user-avatar" />
-                </div>
-                <div className="avatar">
-                  <img src="https://avatarfiles.alphacoders.com/273/thumb-1920-273316.png" alt="user-avatar" />
-                </div>
-                <div className="avatar">
-                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkdwrtLxyENbwPYGwFqskbVNxuEqWX-xLctQ&usqp=CAU" alt="user-avatar" />
-                </div>
-              </div>
-            </Feed.Event>
-
-            <Feed.Event style={{ background: "#C0D3EE" }}>
-              <Feed.Label image={dogImg1} />
-              <Feed.Content>
-                <Feed.Date content="1 days ago" />
-                <Feed.Summary>Hello?</Feed.Summary>
-              </Feed.Content>
-              <div className="usersCommented">
-                <div className="avatar">
-                  <div className="avatar">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkdwrtLxyENbwPYGwFqskbVNxuEqWX-xLctQ&usqp=CAU" alt="user-avatar" />
-                  </div>
-                  <img src="https://cuteprofilepictures.weebly.com/uploads/2/1/8/5/21856578/2598313_orig.jpg" alt="user-avatar" />
-                </div>
-                <div className="avatar">
-                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9W2_KnQG4R-BVSWNpTTJCueKLcjFbwhCsnT3KI92Yhx3B3ZdKrjdzcEeuVWR8y32PaxA&usqp=CAU" alt="user-avatar" />
-                </div>
-                <div className="avatar">
-                  <img
-                    src="https://media1.popsugar-assets.com/files/thumbor/zpzsOsdUEptenTm_qy6IUBOroto/0x0:1660x1660/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2019/10/29/927/n/44701584/0fd997f65db8abf1796548.77940914_/i/short-funny-pet-videos.png"
-                    alt="user-avatar"
-                  />
-                </div>
-                <div className="avatar">
-                  <img src="https://avatarfiles.alphacoders.com/273/thumb-1920-273316.png" alt="user-avatar" />
-                </div>
-              </div>
-            </Feed.Event>
-
-            <Feed.Event style={{ background: "#DDB8B9" }}>
-              <Feed.Label image={dogImg1} />
-              <Feed.Content>
-                <Feed.Date content="2 days ago" />
-                <Feed.Summary>Hello???</Feed.Summary>
-              </Feed.Content>
-              <div className="usersCommented">
-                <div className="avatar">
-                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9W2_KnQG4R-BVSWNpTTJCueKLcjFbwhCsnT3KI92Yhx3B3ZdKrjdzcEeuVWR8y32PaxA&usqp=CAU" alt="user-avatar" />
-                </div>
-                <div className="avatar">
-                  <img src="https://avatarfiles.alphacoders.com/273/thumb-1920-273316.png" alt="user-avatar" />
-                </div>
-                <div className="avatar">
-                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkdwrtLxyENbwPYGwFqskbVNxuEqWX-xLctQ&usqp=CAU" alt="user-avatar" />
-                </div>
-              </div>
-            </Feed.Event>
-
-            <Feed.Event style={{ background: "#AFCFB6" }}>
-              <Feed.Label image={dogImg1} />
-              <Feed.Content>
-                <Feed.Date content="3 days ago" />
-                <Feed.Summary>Anyone here?</Feed.Summary>
-              </Feed.Content>
-              <div className="usersCommented">
-                <div className="avatar">
-                  <img src="https://avatarfiles.alphacoders.com/273/thumb-1920-273316.png" alt="user-avatar" />
-                </div>
-                <div className="avatar">
-                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkdwrtLxyENbwPYGwFqskbVNxuEqWX-xLctQ&usqp=CAU" alt="user-avatar" />
-                </div>
-              </div>
-            </Feed.Event>
-
-            <Feed.Event style={{ background: "#94A2B7" }}>
-              <Feed.Label image={dogImg1} />
-              <Feed.Content>
-                <Feed.Date content="4 day ago" />
-                <Feed.Summary>Feeling good!!!</Feed.Summary>
-              </Feed.Content>
-              <div className="usersCommented">
-                <div className="avatar">
-                  <img src="https://avatarfiles.alphacoders.com/273/thumb-1920-273316.png" alt="user-avatar" />
-                </div>
-                <div className="avatar">
-                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkdwrtLxyENbwPYGwFqskbVNxuEqWX-xLctQ&usqp=CAU" alt="user-avatar" />
-                </div>
-                <div className="avatar">
-                  <img src={dogImg2} alt="user-avatar" />
-                </div>
-                <div className="avatar">
-                  <img
-                    src="https://media1.popsugar-assets.com/files/thumbor/zpzsOsdUEptenTm_qy6IUBOroto/0x0:1660x1660/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2019/10/29/927/n/44701584/0fd997f65db8abf1796548.77940914_/i/short-funny-pet-videos.png"
-                    alt="user-avatar"
-                  />
-                </div>
-                <div className="avatar">
-                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9W2_KnQG4R-BVSWNpTTJCueKLcjFbwhCsnT3KI92Yhx3B3ZdKrjdzcEeuVWR8y32PaxA&usqp=CAU" alt="user-avatar" />
-                </div>
-              </div>
-            </Feed.Event>
-
-            <Feed.Event style={{ background: "#82BEB7" }}>
-              <Feed.Label image={dogImg1} />
-              <Feed.Content>
-                <Feed.Date content="6 days ago" />
-                <Feed.Summary>Anyone wanna go for a walk?</Feed.Summary>
-              </Feed.Content>
-              <div className="usersCommented">
-                <div className="avatar">
-                  <img src="https://avatarfiles.alphacoders.com/273/thumb-1920-273316.png" alt="user-avatar" />
-                </div>
-                <div className="avatar">
-                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkdwrtLxyENbwPYGwFqskbVNxuEqWX-xLctQ&usqp=CAU" alt="user-avatar" />
-                </div>
-              </div>
-            </Feed.Event>
-
-            <Feed.Event style={{ background: "#FEEFD0" }}>
-              <Feed.Label image={dogImg1} />
-              <Feed.Content>
-                <Feed.Date content="7 days ago" />
-                <Feed.Summary>I am living the life!</Feed.Summary>
-              </Feed.Content>
-              <div className="usersCommented">
-                <div className="avatar">
-                  <img src={dogImg2} alt="user-avatar" />
-                </div>
-                <div className="avatar">
-                  <img src="https://cuteprofilepictures.weebly.com/uploads/2/1/8/5/21856578/2598313_orig.jpg" alt="user-avatar" />
-                </div>
-                <div className="avatar">
-                  <img
-                    src="https://media1.popsugar-assets.com/files/thumbor/zpzsOsdUEptenTm_qy6IUBOroto/0x0:1660x1660/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2019/10/29/927/n/44701584/0fd997f65db8abf1796548.77940914_/i/short-funny-pet-videos.png"
-                    alt="user-avatar"
-                  />
-                </div>
-                <div className="avatar">
-                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9W2_KnQG4R-BVSWNpTTJCueKLcjFbwhCsnT3KI92Yhx3B3ZdKrjdzcEeuVWR8y32PaxA&usqp=CAU" alt="user-avatar" />
-                </div>
-                <div className="avatar">
-                  <img src="https://avatarfiles.alphacoders.com/273/thumb-1920-273316.png" alt="user-avatar" />
-                </div>
-              </div>
-            </Feed.Event>
-          </Feed>
-        </Card.Content>
+        <section className="contentContainer">
+          {/* {petProfile.profiles.sort((a, b) => (a.likes.length > b.likes.length ? 1 : -1)).map()} */}
+          {leaderboard.map((profile) => (
+            <div>
+              <h1>
+                {profile.user.name} : {profile.likes.length}
+              </h1>
+            </div>
+          ))}
+        </section>
       </Card>
     </section>
   );
 };
 
-// Posts.propTypes = {};
+Sidebar.propTypes = {
+  auth: PropTypes.object.isRequired,
+  petProfile: PropTypes.object.isRequired,
+  post: PropTypes.object.isRequired,
+};
 
-export default withRouter(Sidebar);
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  petProfile: state.petProfile,
+  post: state.post,
+});
+
+export default connect(mapStateToProps)(withRouter(Sidebar));
+// export default connect(null, { addExperience })(withRouter(AddExperience));
