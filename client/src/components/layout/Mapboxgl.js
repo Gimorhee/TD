9178f6751327795fd99e55712134e221fa8d9567
Danny Rhee <47438772+Gimorhee@users.noreply.github.com>
2021-09-08 19:55:40 -0700

    initial commit for implementing map feature

---
+++ client/src/components/layout/Mapboxgl.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Mapboxgl = (props) => {
+  return <div>MAPBOX</div>;
+};
+
+Mapboxgl.propTypes = {};
+
+export default Mapboxgl;
