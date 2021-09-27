c13914a70738dc7c8311403a9349b6b36f9e1506
Danny Rhee <47438772+Gimorhee@users.noreply.github.com>
2021-09-26 18:20:18 -0700

    debugging

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ app.use("/api/posts", require("./routes/api/posts"));
 if (process.env.node_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
-
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
