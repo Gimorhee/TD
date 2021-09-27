9c5960a863832ca09903fb724e9bb8a017258e66
Danny Rhee <47438772+Gimorhee@users.noreply.github.com>
2021-09-26 18:32:56 -0700

    getting 'cannot get /' error

--- server.js
+++ server.js
@@ -16,14 +16,14 @@ app.use("/api/petProfile", require("./routes/api/petProfile"));
 app.use("/api/posts", require("./routes/api/posts"));
 
 // Serve static assets in production
-if (process.env.node_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
+
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
