9571e6012918ed780ea8592c11f7c7b51e3af205
Danny Rhee <47438772+Gimorhee@users.noreply.github.com>
2021-09-26 18:12:03 -0700

    Prepared for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db.js");
 const app = express();
+const path = require("path");
 
 // Connect Database
 connectDB();
@@ -8,15 +9,21 @@ connectDB();
 // Init Middleware (BodyParser)
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.send("API Running");
-});
-
 // Defined Routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/petProfile", require("./routes/api/petProfile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+// Serve static assets in production
+if (process.env.node_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
